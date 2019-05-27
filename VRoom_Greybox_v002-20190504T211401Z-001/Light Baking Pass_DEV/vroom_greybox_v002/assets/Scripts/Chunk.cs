993b94e158177ac7ad36b6639d3b0079cbf22353
jeraiii <44616099+jeraiii@users.noreply.github.com>
2019-05-26 20:12:41 -0400

    Dev (#4)
    * Matt edits
    
    Got my organization added to the recent version from sunday. Have chunk loading working. Renamed to alpha version
    
    * Chunk loading
    
    Chunk loading functioning in game have to fine tune placement of collision barriers
    
    * updated alpha scene
    
    fine tuned where chunk loading occured when bike moved distance threshold should be set to 35 and scene will run fine
    
    * d
    
    d
    
    * Updated scene, WORK FROM HERE
    
    This project folder should have a scene labeled "BnP_Alpha" PLEASE WORK FROM HERE!

---
+++ VRoom_Greybox_v002-20190504T211401Z-001/Light Baking Pass_DEV/vroom_greybox_v002/assets/Scripts/Chunk.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    List<GameObject> taggedObjsInScene;
+    public Transform viewer;
+    public int viewDist;
+    Vector3 viewerPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("chunkable");
+        taggedObjsInScene = new List<GameObject>(objs);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        viewerPosition = new Vector3(viewer.position.x, viewer.position.y, viewer.position.z);
+        CheckEnabledDisabled();
+
+
+
+    }
+    void CheckEnabledDisabled()
+    {
+        for (int i = 0; i < taggedObjsInScene.Count; i++)
+        {
+            if (Vector3.Distance(viewerPosition, taggedObjsInScene[i].transform.position) <= viewDist)
+            {
+                taggedObjsInScene[i].SetActive(true);
+
+            }
+            else
+            {
+                taggedObjsInScene[i].SetActive(false);
+            }
+        }
+
+
+
+
+    }
+}
